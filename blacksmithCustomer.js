4274b458a01a32cb89bdfae1a5720d8eec637f9b
Ethan <ebonsignori@gmail.com>
2019-05-08 18:02:45 -0400

    added gossipping for fun!

--- blacksmithCustomer.js
+++ blacksmithCustomer.js
@@ -3,25 +3,22 @@ const inquirer = require('inquirer')
 
 const log = console.log
 
+// MySQL connection options
 const connection = mysql.createConnection({
   host: 'localhost',
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: 'root',
-
-  // Your password
   password: 'root',
   database: 'blacksmith'
 })
 
+// Entry point, starts the app
 connection.connect(err => {
   if (err) throw err
   enterBlacksmith()
 })
 
+// Enter shop
 function enterBlacksmith () {
   inquirer.prompt({
     name: 'enter',
@@ -38,6 +35,7 @@ function enterBlacksmith () {
   })
 }
 
+// Show stock of items and options for what user can do next
 function showBlacksmith () {
   // Display random line of dialogue from the array below
   const dialogueArray = ['"May I suggest a shiny new iron sword? My son just forged that sharp one on the end."', '"Have I seen you before?..."', '"The innkeeper may have a quest for you. I think a new sword would help!"', `"Hey, if these are too expensive for you, I've got some 'used' swords in the back..."`, '"I used to be an adventurer like you, then I took an arrow to the knee."', '"Some people call this junk. Me? I call it treasure."', '"Looking to protect yourself? Or deal some damage?"', `"Looking to buy? I've got quality arms."`, '"Everyone needs protection and I offer protection at little cost!"', '"Do you have a permit to carry a sword?"', '"Even the strong need protection! I suggest one of my fine swords."', `"I've got plenty of materials if you are looking to forge your own blade."`, '"Just got a shipment of ore from the mines in. Quality stuff!"']
@@ -53,11 +51,13 @@ function showBlacksmith () {
       name: 'buy',
       message: 'What would you like to do?',
       type: 'list',
-      choices: ['Buy', 'Leave Blacksmith [Exit App]']
+      choices: ['Buy', 'Gossip', 'Leave Blacksmith [Exit App]']
     }).then(choice => {
       switch (choice.buy) {
         case 'Buy':
           return buyItem()
+        case 'Gossip':
+          return gossip()
         case 'Leave Blacksmith [Exit App]':
           exitApp()
       }
@@ -102,6 +102,18 @@ function buyItem () {
       })
     })
   })
+}
+
+// Holds the current index for which gossip option to chose
+let gossipIndex = 0
+// Shows a line of dialogue based on how many times a user has pressed gossip
+function gossip () {
+  const gossipArray = [`"I heard the inkeeper was looking for an adventurer, maybe you should go talk with him!"`, `"A little tip... I wouldn't stay at the Red Frog Inn anytime soon. Word is they've got a necromancer living in the basement!"`, `"Little Macy came sprinting through the West Gate yesterday shouting "GOBLINS! GOBLINS ARE ATTACKING!! Whole town got up in arms when it turns out it was just her trouble making brother in a mask!`, `"Did you hear that Earl Westhoof's manor was robbed? I wonder who did it..."`, `"A few night's ago an Orc came in here looking for a blade. Can you believe it? AN ORC!"`, `"Have you heard? Someone dug up old Herbert Longfoot's grave! Now people are telling tales of a ghost haunting the cemetary!"`, `"If you're looking for armor, you should speak with Redford a few shops down. Tell him I sent you!"`, `"I haven't heard much else... Less talk more shop, eh?"`]
+  log(`\n\n${gossipArray[gossipIndex]}\n`)
+  gossipIndex++
+  // Set the index to the last index of the array if it goes past it
+  if (gossipIndex >= gossipArray.length) gossipIndex = gossipArray.length - 1
+  goBack('"Would you like to buy anything?"')
 }
 
 // Generic 'go back' list prompt, message altered by function argument
